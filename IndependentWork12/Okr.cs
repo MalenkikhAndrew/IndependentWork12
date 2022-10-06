dbba945397c1879a4406e71975d66412abc6b09d
User <User@DESKTOP-A50L8A1>
2022-10-07 09:56:06 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork12/Okr.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork12
+{
+    internal class Okr
+    {
+        static public double GetDlinOkr(double r) 
+        {
+            return 2*Math.PI*r;
+        }
+        static public double GetPloshOkr(double r)
+        {
+            return Math.PI * Math.Pow(r,2);
+        }
+        static public double GetPrinodlesh(double r, double x, double y)
+        { 
+          
+            return Math.Sqrt(Math.Pow(x,2)+ Math.Pow(y, 2));
+  
+        }
+    }
+}
---
+++ IndependentWork12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ввести радиус");
+            double r=Convert.ToDouble(Console.ReadLine());
+            double l_ = Okr.GetDlinOkr(r);
+            double s_ = Okr.GetPloshOkr(r);
+            Console.WriteLine("Ввести X,Y");
+            double x=Convert.ToDouble(Console.ReadLine());
+            double y=Convert.ToDouble(Console.ReadLine());
+            double p_ = Okr.GetPrinodlesh(r, x, y);
+            
+                Console.WriteLine($"1)Длина окружности L={l_:.00} \n2)Площадь окружности S={s_:.00} \n3)Гепатинуза H={p_:.00}");
+            if (p_ <= r)
+            {
+                Console.WriteLine("Точка принадлежит кругу");
+            }
+            else
+            {
+                Console.WriteLine("Точка не принадлежит кругу"); 
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+
+    }
+}
